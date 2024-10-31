4f9ae4ac102cd623c6f136105767816e3cacdbd2
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-10-31 14:33:35 +0100

    bump version

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@
  * ---------------------------------------------------------------------
  */
 
-define('PLUGIN_OKTA_VERSION', '1.4.3');
+define('PLUGIN_OKTA_VERSION', '1.4.4');
 
 if (!defined("PLUGIN_OKTA_DIR")) {
    define("PLUGIN_OKTA_DIR", Plugin::getPhpDir("okta"));
